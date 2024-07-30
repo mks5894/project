d722196302f67b48777bd9ca436f9808e193a027
mks5894 <mks5894@nate.com>
2024-07-30 13:28:26 +0900

    Add files via upload

--- project.sql
+++ project.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `board`
 --
 
+CREATE DATABASE IF NOT EXISTS project;
+USE project;
+
 DROP TABLE IF EXISTS `board`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
